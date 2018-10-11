f7ca59cd4c965e57c2ecc854bf8e485cad20755b
Steve Erhart <steve.erhart@dudesolutions.com>
2018-10-11 18:32:38 -0400

    inc added

--- src/musicbrainz.scm
+++ src/musicbrainz.scm
@@ -10,14 +10,37 @@
   #:use-module (ice-9 regex)
   #:use-module (ice-9 receive))
 
+(define mb-ws-host "musicbrainz.org")
+(define mb-ws-path '("ws" "2"))
+(define mb-ws-scheme 'https)
+(define mb-ws-namespace "http://musicbrainz.org/ns/mmd-2.0#")
+
+(define user-agent-header "guile-musicbrainz/0.1.0 (steve@ayerh.art)")
+(define accept-header '((application/xml)))
+
 (define-class <mb-entity> ()
   (mbid #:accessor mbid #:init-keyword #:mbid #:init-value #f))
 
-(define-method (display (self <mb-entity>) port)
-  (format port "#<<mb-entity> ~a>" (mbid self)))
+(define-method (display (entity <mb-entity>) port)
+  (format port "#<<mb-entity> ~a>" (mbid entity)))
 
-(define-method (write (self <mb-entity>) port)
-  (format port "#<<mb-entity> ~a>" (mbid self)))
+(define-method (write (entity <mb-entity>) port)
+  (format port "#<<mb-~a> ~a>" (mb-entity-name entity) (mbid entity)))
+
+(define-method (lookup (entity <mb-entity>) (inc <list>))
+  (mb-entity-response->entity
+   (mb-request
+    inc
+    (mb-entity-name entity)
+    (mbid entity))))
+
+(define-method (lookup (entity <mb-entity>))
+  (lookup entity '()))
+
+(define-method (mb-entity-name (self <mb-entity>))
+  (let ((class-name ((compose symbol->string class-name class-of) self)))
+    (regexp-substitute #f (string-match "<mb-([a-z]+)>" class-name) 1)))
+
 
 (define-class <mb-artist> (<mb-entity>)
   (name #:accessor name #:init-keyword #:name)
@@ -31,33 +54,25 @@
   (disambiguation #:accessor disambiguation #:init-keyword #:isambiguation)
   (annotation #:accessor annotation #:init-keyword #:nnotation))
 
-(define-method (mb-entity-name (self <mb-entity>))
-  (let ((class-name ((compose symbol->string class-name class-of) self)))
-    (regexp-substitute #f (string-match "<mb-([a-z]+)>" class-name) 1)))
-
-(define mb-ws-host "musicbrainz.org")
-(define mb-ws-path '("ws" "2"))
-(define mb-ws-scheme 'https)
-(define mb-ws-namespace "http://musicbrainz.org/ns/mmd-2.0#")
-
-(define user-agent-header "guile-musicbrainz/0.1.0 (steve@ayerh.art)")
-(define accept-header '((application/xml)))
-
-(define (build-mb-uri parts)
-  (display parts)
+(define* (build-mb-uri parts #:optional (inc '()))
   (let* ((path-parts (append mb-ws-path parts))
          (path (string-append "/" (string-join path-parts "/"))))
     (build-uri
      mb-ws-scheme
+     #:query (string-append "inc=" (string-join inc "+"))
      #:host mb-ws-host
      #:path path)))
 
-(define (mb-request . parts)
+(define* (mb-request #:optional (inc '())  #:rest parts)
+  (display parts)
+  (newline)
+  (display inc)
+  (newline)
   (let ((headers `((accept . ,accept-header)
                    (user-agent . ,user-agent-header))))
     (receive (response body)
         (http-get
-         (build-mb-uri parts)
+         (build-mb-uri parts inc)
          #:streaming? #t
          #:headers headers)
       ; TODO: inspect response? adjust rate limiting? etc.
@@ -66,15 +81,6 @@
        (λ (p)
          (xml->sxml p #:namespaces `((mb . ,mb-ws-namespace))))))))
 
-(define-method (lookup (entity <mb-entity>))
-  (mb-entity-response->entity
-   (mb-request
-    (mb-entity-name entity)
-    (mbid entity))))
-
-(define-method (lookup (entity <mb-entity>) (inc <list>))
-  (lookup entity))
-
 (define deniro
   (make <mb-artist>
     #:mbid "f01846dc-1585-401c-a46a-d0b3a824114a"))
@@ -82,18 +88,21 @@
 (define (mb-entity-response->entity sxml)
   (define (parse-artist artist)
     (sxml-match artist
-                [(mb:artist (@ (id ,mbid)
-                               (type-id ,type-id)
-                               (type ,type))
-                            (mb:name ,name)
-                            (mb:sort-name ,sort-name) . ,rest)
+                [(mb:artist
+                  (@ (id ,mbid)
+                     (type-id ,type-id)
+                     (type ,type))
+                  (mb:name ,name)
+                  (mb:sort-name ,sort-name) . ,rest)
                 (make <mb-artist>
                   #:mbid mbid
                   #:name name
                   #:sort-name sort-name)]))
+
   (define (parse-entity entity)
     (sxml-match entity
                 [,[parse-artist -> artist] artist]))
+
   (sxml-match sxml
               [(*TOP*
                 (*PI* . ,pi)
